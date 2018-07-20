de3eed94133c9ca033c37d0e21baf6c889b96301
Clement Fourage <clement-fourage@hotmail.fr>
2018-07-20 15:04:19 +0200

    TP 6.5

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -26,28 +26,24 @@ public class PizzeriaAdminConsoleApp {
 			case 1:
 				ListerPizzasService lister = new ListerPizzasService();
 				lister.executeUC(scan, dao);
-				
 				break;
 
 				//Ajout d'une nouvelle pizza dans le tableau existant	
 			case 2:
 				AjouterPizzaService ajouter = new AjouterPizzaService();
 				ajouter.executeUC(scan, dao);
-				
 				break;
 
 				//Modification d'une pizza dans le tableau existant
 			case 3:
 				ModifierPizzaService modifier = new ModifierPizzaService();
 				modifier.executeUC(scan, dao);
-				
 				break;
 
 				//Supprimer une pizza
 			case 4:
 				SupprimerPizzaService supprimer = new SupprimerPizzaService();
 				supprimer.executeUC(scan, dao);
-				
 				break;
 
 			case 99:
